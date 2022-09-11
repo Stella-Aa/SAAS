d9e15cbed2d57a7ca5da94b3e49f90c3c1785277
Stella-Aa <111791958+Stella-Aa@users.noreply.github.com>
2022-09-12 01:01:17 +0400

    Create ItDoesn'tWork.js

---
+++ ItDoesn'tWork.js
@@ -0,0 +1,16 @@
+# SAAS
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8010;
+
+// sendFile will go here
+app.get("/Image/:JpgName", (req,res) => {
+  let JpgName = req.params["JpgName"];
+  res.sendFile(path.join(__dirname, '/Image/:JpgName'));
+  console.log(JpgName);
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
