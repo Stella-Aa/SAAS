a80819b80388d00074a290a3e6a7b696c2946f23
Stella-Aa <111791958+Stella-Aa@users.noreply.github.com>
2022-09-25 20:13:25 +0400

    Create 7)minecraft.js

---
+++ 7)minecraft.js
@@ -0,0 +1,50 @@
+# SAAS
+const express = require("express");
+const app = express();
+const port = process.env.port || 4443;
+const HTML_BLANK = "<!DOCTYPE html> <html lang='en'> <head> <meta charset='UTF-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> <meta name='viewport' content='width=device-width, initial-scale=1.0'> <title>Gago</title> </head><body> OURTABLEHEARE </body></html>"
+
+
+function createTable(n) {
+
+  console.log("TABLE")
+  let table = "<table border='10' width = 800px height = 800px align='center'>"
+  for (let i = 0; i < 16; i++) {
+    table += "<tr>"
+    for (let j = 0; j < 16; j++) {
+      
+      
+     var a=Math.floor(Math.random() * 7);     
+     if (a==0||a==1||a==2) {
+      
+      table += "<td align='center' colspan='1' style=background-color:Red> </td> "
+     }     
+     if (a==3||a==4||a==5) {
+      table += "<td align='center' colspan='1' style=background-color:Green> </td> "
+     }  
+     if (a==6) {
+      table += "<td align='center' colspan='1' style=background-color:DodgerBlue> </td> "
+      
+     }  
+     
+    }
+   
+    table += "</tr>"
+  }
+  table += "</table>"
+  const HTML = HTML_BLANK.replace('OURTABLEHEARE', table)
+  return HTML
+}
+
+app.get("/", (req, res) => {
+
+  const board = createTable()
+  res.send(board)
+});
+app.listen(port, err => {
+  if (err) {
+    return Console.log("Error", err);
+
+  }
+  console.log(`Listening on port ${port}`);
+});
